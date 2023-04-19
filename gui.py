79bec4253d4ab6ef50f80c08b205f5e5b74bc32a
Rafael N <rafaelngoncalves5@outlook.com>
2023-04-18 22:28:18 -0300

    Fix

--- gui.py
+++ gui.py
@@ -1,35 +1,22 @@
 import tkinter as tk
 from tkinter import font
-import test
 
-class ResultWindow(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid()
-        self.createWidgets()
-
-    def createWidgets(self):
-
-        self.titulo = tk.Label(self, font=('Courier', 20, 'bold'), text="Resultado", pady=30)
-        self.titulo.grid()
-
+window = tk.Tk()
+window.resizable(False, False)
+window.geometry('400x500')
 
 class Application(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master=window):
         tk.Frame.__init__(self, master)
-        self.grid()
+        self.grid(column=4)
+        
         self.createWidgets()
+        # Tem de chamar esse método pra funcionar o width e o height do Frame
+        #self.grid_propagate(False)
 
     # Aqui a gente realiza o cálculo
     def calcular(self):
         print('Estou calculando...')
-        for f in font.families():
-            print(f)
-
-        # Instancio uma janela com o resultado
-        rw = ResultWindow()                     
-        rw.master.title('Resultado')
-        rw.mainloop()
 
     def createWidgets(self):
 
@@ -37,16 +24,19 @@ class Application(tk.Frame):
         self.titulo.grid()
 
         self.input_box = tk.Entry(self, width=25)
-        self.input_box.grid()
+        self.input_box.grid(columnspan=1)
 
         self.input_box_2 = tk.Entry(self, width=25)
-        self.input_box_2.grid()
+        self.input_box_2.grid(columnspan=1)
+
+        self.input_box_3 = tk.Entry(self, width=5)
+        self.input_box_3.grid(columnspan=1)
 
         self.calcular_btn = tk.Button(self, text="Calcular", background='blue', justify=tk.CENTER, 
                                       command=self.calcular, font=("Courier", 16, 'bold'), fg='white', 
                                       relief='raised', width=25)
         self.calcular_btn.grid()
 
-app = Application()                     
+app = Application()  
 app.master.title('Prop incerteza')
 app.mainloop()
