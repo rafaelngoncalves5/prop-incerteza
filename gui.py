2cbf812382bce591dd0c5bcc9646c1a49813149b
Rafael N <rafaelngoncalves5@outlook.com>
2023-04-20 21:15:09 -0300

    Fix

--- gui.py
+++ gui.py
@@ -4,11 +4,11 @@ from time import sleep
 
 class ResultWindow(tk.Frame):
     result = str
-    stuff_code = str
+    latex = str
 
-    def __init__(self, master=None, result=result, stuff_code = stuff_code):
+    def __init__(self, master=None, result=result, latex = latex):
         self.result = result
-        self.stuff_code = stuff_code
+        self.latex = latex
         tk.Frame.__init__(self, master, background='grey') 
         self.grid()
         self.createWidgets()
@@ -21,14 +21,14 @@ class ResultWindow(tk.Frame):
                                     background='cyan', padx=20, pady=20)
         self.show_result.grid(column=1, row=1)
 
-        self.titulo_stuff_code = tk.Label(self, foreground='white', background='grey', text="Stuff code", font=('Courier', 15, 'bold'))
-        self.titulo_stuff_code.grid(column=1, row=2, pady=30)
+        self.titulo_latex = tk.Label(self, foreground='white', background='grey', text="Latex", font=('Courier', 15, 'bold'))
+        self.titulo_latex.grid(column=1, row=2, pady=30)
 
-        self.show_stuff_code = tk.Label(self, text=str(self.result), font=('Courier', 12, 'bold'), 
+        self.show_latex = tk.Label(self, text=str(self.result), font=('Courier', 12, 'bold'), 
                                     background='orange', padx=20, pady=20)
-        self.show_stuff_code.grid(column=1, row=3)
+        self.show_latex.grid(column=1, row=3)
 
-count = 2
+count = 4
 
 window = tk.Tk()
 window.resizable(False, False)
@@ -49,60 +49,82 @@ class Application(tk.Frame):
         global count
         count += 1  
 
-        self.eq_input = tk.Entry(self, bd=5)
-        self.eq_input.grid(column=0, row=count, padx=20)     
-
         self.var_input = tk.Entry(self, bd=5)
-        self.var_input.grid(column=1, row=count, padx=20)
+        self.var_input.grid(column=0, row=count, padx=20)     
+
+        self.val_input = tk.Entry(self, bd=5)
+        self.val_input.grid(column=1, row=count, padx=20)
         
-        self.stuff_input = tk.Entry(self, bd=5)
-        self.stuff_input.grid(column=2, row=count, padx=20)
+        self.inc_input = tk.Entry(self, bd=5)
+        self.inc_input.grid(column=2, row=count, padx=20)
 
         # Adiciona numa DS pra realizar os cálculos
         # ...
 
     # Aqui a gente realiza o cálculo
     def calcular(self):
-        print('Estou calculando...')
-        sleep(1)
+
         
+
+        # Realiza o cálculo em si:
+        # ...
+        
+
+
+
+
+
+
+
+
+
+        # Retorna o resultado
         self.result = 'dsf32423423942034023042dvbv453443dsfsd343423tvb'
 
-        self.stuff_code = 'dfinsdkfnsdfngfkfgn'
+        self.latex = 'dfinsdkfnsdfngfkfgn'
 
         # Abre a janela de resultado
         res_window = tk.Tk()
         res_window.resizable(False, False)
-        result_window = ResultWindow(master=res_window, result=self.result, stuff_code=self.stuff_code)
+        result_window = ResultWindow(master=res_window, result=self.result, latex=self.latex)
         result_window.master.title('Resultado')
         result_window.mainloop()
 
-        return self.result + self.stuff_code
+        return self.result + self.latex
 
     def createWidgets(self):
+        # Só existe uma equação:            
+        def grab_value(e, *args):
+            # Pegando a equação
+            equation = self.eq_input.get()
 
         self.titulo = tk.Label(self, text="Prop incerteza", font=('Courier', 15, 'bold'))
         self.titulo.grid(column=1, row=0, pady=30)
 
-        self.add_input_btn = tk.Button(self, text="+", background='green', 
-                                      command=self.create_input, font=("Courier", 8, 'bold'), fg='white', 
-                                      relief='flat')
-        self.add_input_btn.grid(column=2, row=0)
-
-        self.eq_text = tk.Label(self, text="Equação")
-        self.eq_text.grid(column=0, row=1)
+        self.eq_text = tk.Label(self, text="Equação", font=('Courier', 12)).grid(column=1, row=1)
         self.eq_input = tk.Entry(self, bd=5)
-        self.eq_input.grid(column=0, row=2, padx=20)     
+        self.eq_input.grid(column=1, row=2)
+        self.eq_input.bind('<Key>', grab_value)
+
+        self.add_input_btn = tk.Button(self, text="+", background='green', 
+                                      command=self.create_input, fg='white', 
+                                      relief='flat', padx=5)
+        self.add_input_btn.grid(column=1, row=39)
 
         self.var_text = tk.Label(self, text="Variável")
-        self.var_text.grid(column=1, row=1)
+        self.var_text.grid(column=0, row=3)
         self.var_input = tk.Entry(self, bd=5)
-        self.var_input.grid(column=1, row=2, padx=20)
+        self.var_input.grid(column=0, row=4, padx=20)
+
+        self.val_text = tk.Label(self, text="Valor")
+        self.val_text.grid(column=1, row=3)
+        self.val_input = tk.Entry(self, bd=5)
+        self.val_input.grid(column=1, row=4, padx=20)
         
-        self.stuff_text = tk.Label(self, text="Stuff")
-        self.stuff_text.grid(column=2, row=1)
-        self.stuff_input = tk.Entry(self, bd=5)
-        self.stuff_input.grid(column=2, row=2, padx=20)
+        self.inc_text = tk.Label(self, text="Incerteza")
+        self.inc_text.grid(column=2, row=3)
+        self.inc_input = tk.Entry(self, bd=5)
+        self.inc_input.grid(column=2, row=4, padx=20)
 
         self.calcular_btn = tk.Button(self, text="Calcular", background='blue', 
                                       command=self.calcular, font=("Courier", 16, 'bold'), fg='white', 
