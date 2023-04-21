c070fe674299377670083afa6f968680ece97279
Rafael N <rafaelngoncalves5@outlook.com>
2023-04-21 19:48:35 -0300

    Resolvido o bug

--- gui.py
+++ gui.py
@@ -6,9 +6,9 @@ var_list = list()
 val_list = list()
 inc_list = list()
 
-var1 = str
-val1 = str
-inc1 = str
+svar_list = list()
+sval_list = list()
+sinc_list = list()
 
 def calcular(equation, **kwargs):
     # Pegando a equação. Só existe uma equação
@@ -16,6 +16,7 @@ def calcular(equation, **kwargs):
     # Variáveis(var), valores(val) e incertezas(inc)
     print(kwargs)
 
+    # Limpa a lista
     var_list.clear()
     val_list.clear()
     inc_list.clear()
@@ -52,11 +53,6 @@ window = tk.Tk()
 window.resizable(False, False)
 #window.geometry('400x500')
 
-def save():
-    var_list.append(var1)
-    val_list.append(val1)
-    inc_list.append(inc1)
-
 class Application(tk.Frame):
 
     def __init__(self, master=window):
@@ -66,7 +62,6 @@ class Application(tk.Frame):
         # Tem de chamar esse método pra funcionar o width e o height do Frame
         # self.grid_propagate(False)
 
-    # Aqui a gente realiza o cálculo
     def grab_value(self):
 
         # Insere na lista de variáveis
@@ -80,6 +75,16 @@ class Application(tk.Frame):
         # Insere na lista de incertezas
         inc_list.append(self.inc_input.get())
         inc_list.append(self.inc2_input.get())
+        
+        # Salva tudo
+        for variable in svar_list:
+            var_list.append(variable.get())
+
+        for value in sval_list:
+            val_list.append(value.get())
+
+        for uncertainty in sinc_list:
+            inc_list.append(uncertainty.get())
 
         # Realiza o cálculo em si:
         calcular(
@@ -104,34 +109,22 @@ class Application(tk.Frame):
         # return self.result + self.latex
 
     def create_input(self):
-
-        def get_values(e):
-            global var1
-            global val1
-            global inc1
-
-            var1 = self.var1.get()
-            val1 = self.val1.get()
-            inc1 = self.inc1.get()
-
-            print(var1, val1, inc1)
-        
         global count
         count += 1
          
         self.var1 = tk.Entry(self, bd=5)
         self.var1.grid(column=0, row=count, padx=20)
-        self.var1.bind('<Key>', get_values)
 
         self.val1 = tk.Entry(self,bd=5)
         self.val1.grid(column=1, row=count)
-        self.var1.bind('<Key>', get_values)
 
         self.inc1 = tk.Entry(self,bd=5)
         self.inc1.grid(column=2, row=count)
-        self.inc1.bind('<Key>', get_values)
 
-        save()
+        svar_list.append(self.var1)
+        sval_list.append(self.val1)
+        sinc_list.append(self.inc1)
+
 
     def createWidgets(self):
 
